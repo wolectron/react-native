c5cb586d5bb402b65653618a65ee68e2da2c1c4f
bharath <bharath.shetru@videoelephant.com>
2020-12-27 18:59:11 +0000

    fixing the homepage carousel

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,102 +1,47 @@
-import React, { Component } from 'react';
-import { Text, View, Dimensions, StyleSheet } from 'react-native';
-import {FlatListSlider} from 'react-native-flatlist-slider';
-
-import Carousel from 'react-native-snap-carousel'; // Version can be specified in package.json
-
-import { scrollInterpolator, animatedStyles } from '../utils/animations';
-
-const SLIDER_WIDTH = Dimensions.get('window').width; 
-const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.5);
-const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
-const SLIDER_HEIGHT = ITEM_HEIGHT;
+import React from 'react';
+import { StyleSheet, SafeAreaView, Platform, StatusBar, Alert } from 'react-native';
+import { FlatListSlider } from 'react-native-flatlist-slider';
+import Preview from "../components/Preview";
 
 const images = [
   {
    image:'https://images.unsplash.com/photo-1567226475328-9d6baaf565cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
-   desc: 'Silent Waters in the mountains in midst of Himilayas',
+   desc: 'Silent Waters in the mountains in midst of Himalayas',
   },
- {
-   image:'https://images.unsplash.com/photo-1455620611406-966ca6889d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1130&q=80',
-   desc:
-     'Red fort in India New Delhi is a magnificient masterpeiece of humans',
- },
+  {
+    image:'https://images.unsplash.com/photo-1455620611406-966ca6889d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1130&q=80',
+    desc: 'Red fort in India New Delhi is a magnificient masterpeiece of humans',
+  }
  ]
 
 function HomeScreen(props) {
- const [index, setIndex] = React.useState(0);
-
- const DATA = [];
-for (let i = 0; i < 10; i++) {
-  DATA.push(i)
-}
-
-    console.log(index); 
-  function _renderItem({ item }) {
-    return (
-      <View style={styles.itemContainer}>
-        <Text style={styles.itemLabel}>{`Item ${item}`}</Text>
-      </View>
-    );
-  }
-  
-    return (
-      <View
-        //style={styles.carouselView}     
-      >
-        
-        
-        <FlatListSlider 
-          data={images}
-          width={275}
-          onPress={item => alert(JSON.stringify(item))}
-          indicatorActiveWidth={40}
-          contentContainerStyle={{paddingHorizontal: 16}}
-          separatorWidth={25}
-          autoscroll={false}
-          animation={false}
-        />
-
-        <FlatListSlider 
-          data={images}
-          width={275}
-          onPress={item => alert(JSON.stringify(item))}
-          indicatorActiveWidth={40}
-          contentContainerStyle={{paddingHorizontal: 16}}
-          separatorWidth={25}
-          autoscroll={false}
-          animation={false}
-        />
-      </View>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatListSlider
+        data={images}
+        width={275}
+        component={<Preview />}
+        onPress={item => alert(JSON.stringify(item))}
+        indicatorActiveWidth={40}
+        contentContainerStyle={{paddingHorizontal: 16}}
+      />
+      <FlatListSlider
+        data={images}
+        width={275}
+        component={<Preview />}
+        onPress={item => alert(JSON.stringify(item))}
+        indicatorActiveWidth={40}
+        contentContainerStyle={{paddingHorizontal: 16}}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  carouselContainer: {
-    marginTop: 50,
-  },
-  itemContainer: {
-    width: ITEM_WIDTH,
-    height: ITEM_HEIGHT,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'dodgerblue'
-  },
-  itemLabel: {
-    color: 'white',
-    fontSize: 24
-  },
-  counter: {
-    marginTop: 25,
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  carouselView:{
-      marginTop: 200,
-      alignItems: 'flex-start',
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   }
 });
-
 
 export default HomeScreen;
