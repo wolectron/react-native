5cde34c8ab13d4945bbc0066e4739f0c29bc9ff1
Harinandan Srinivasamurthy <harinandansrinivasamurthy@Harinandans-MacBook-Pro.local>
2020-12-27 09:45:13 -0800

    diff carouusel

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import {FlatListSlider} from 'react-native-flatlist-slider';
 
 import Carousel from 'react-native-snap-carousel'; // Version can be specified in package.json
 
 import { scrollInterpolator, animatedStyles } from '../utils/animations';
 
 const SLIDER_WIDTH = Dimensions.get('window').width; 
-const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.3);
+const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.5);
 const ITEM_HEIGHT = Math.round(ITEM_WIDTH * 3 / 4);
 const SLIDER_HEIGHT = ITEM_HEIGHT;
 
-
+const images = [
+  {
+   image:'https://images.unsplash.com/photo-1567226475328-9d6baaf565cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+   desc: 'Silent Waters in the mountains in midst of Himilayas',
+  },
+ {
+   image:'https://images.unsplash.com/photo-1455620611406-966ca6889d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1130&q=80',
+   desc:
+     'Red fort in India New Delhi is a magnificient masterpeiece of humans',
+ },
+ ]
 
 function HomeScreen(props) {
  const [index, setIndex] = React.useState(0);
@@ -31,35 +42,30 @@ for (let i = 0; i < 10; i++) {
   
     return (
       <View
-        style={styles.carouselView}     
+        //style={styles.carouselView}     
       >
-        <Carousel
-          ref={(c) => carousel = c}
-          data={DATA}
-          renderItem={({item}) => _renderItem({item})}
-          sliderWidth={SLIDER_WIDTH}
-          sliderHeight={SLIDER_HEIGHT}
-          itemWidth={ITEM_WIDTH}
-          containerCustomStyle={styles.carouselContainer}
-          inactiveSlideShift={0}
-          onSnapToItem={(i) => setIndex({ i })}
-          scrollInterpolator={scrollInterpolator}
-          slideInterpolatedStyle={animatedStyles}
-          useScrollView={true}
-          layoutCardOffset={2}     
+        
+        
+        <FlatListSlider 
+          data={images}
+          width={275}
+          onPress={item => alert(JSON.stringify(item))}
+          indicatorActiveWidth={40}
+          contentContainerStyle={{paddingHorizontal: 16}}
+          separatorWidth={25}
+          autoscroll={false}
+          animation={false}
         />
-        <Carousel
-          ref={(c) => carousel = c}
-          data={DATA}
-          renderItem={({item}) => _renderItem({item})}
-          sliderWidth={SLIDER_WIDTH}
-          itemWidth={ITEM_WIDTH}
-          containerCustomStyle={styles.carouselContainer}
-          inactiveSlideShift={0}
-          onSnapToItem={(i) => setIndex({ i })}
-          scrollInterpolator={scrollInterpolator}
-          slideInterpolatedStyle={animatedStyles}
-          useScrollView={true}          
+
+        <FlatListSlider 
+          data={images}
+          width={275}
+          onPress={item => alert(JSON.stringify(item))}
+          indicatorActiveWidth={40}
+          contentContainerStyle={{paddingHorizontal: 16}}
+          separatorWidth={25}
+          autoscroll={false}
+          animation={false}
         />
       </View>
     );
